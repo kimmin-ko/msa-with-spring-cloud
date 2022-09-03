9edfb44a3eaba215ec3b53e969d7a4a98bf0c3c0
kimmin <kimmin2301@gmail.com>
2022-09-03 17:57:09 +0900

    feat: lb routing

--- firstservice/src/main/java/com/example/firstservice/FirstController.java
+++ firstservice/src/main/java/com/example/firstservice/FirstController.java
@@ -1,15 +1,22 @@
 package com.example.firstservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,8 +30,10 @@ public class FirstController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server Port: {}", request.getServerPort());
+        return String.format("Hi there. This is a message from First Service. Server Port: %s",
+                env.getProperty("local.server.port"));
     }
 
 }
